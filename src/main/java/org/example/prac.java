40245f0a3f558126f9bb1486dead1ef65bf110b0
깃hrjo0120 <hrjo0120@gmail.com>
2024-05-31 13:05:52 +0900

    work2

---
+++ src/main/java/org/example/prac.java
@@ -0,0 +1,70 @@
+//0531
+// 자바의 타입(자료형)
+// 1. 기본(있는)
+// 2. 참조(없는)
+
+package org.example;
+
+public class prac {
+    public static void main(String[] args) {
+//        //논리(true, false) -> boolean
+//        boolean b = true; // 1 > 2 // 2 > 1
+//        // 문자
+//        char c = 65 + 2;
+//        // 정수
+//        byte bb = 40; // /128~127
+//        System.out.println(bb);
+//        short s = 40; // 약 -32000 ~ +32000
+//        System.out.println(s);
+//        int i = 40; // 약 -21억 ~ + 21억
+//        System.out.println(i);
+//        long l = 40L; // 그 이상 40L로 원래는 써야하지만 자동이 변환이 되어서 들어간다.
+//        System.out.println(l);
+//
+//        // 실수
+//        float f = 3.14f; // 4바이트
+//        double d = 3.14; // 8바이트
+//
+//        String ss = "Hi";
+
+//        byte b = 127; //~128~127 => 128일때는 저장을 못함. 왜냐? 범위를 벗어낫기 때문에
+//        System.out.println(b);
+
+//        int base = 3, exponent = 4;
+//        long result = 1;
+//
+//        for (; exponent != 0; --exponent) {
+//            result *= base;
+//        }
+//
+//        System.out.println("Answer : " + result);
+
+//        //피보나치 수열
+//        int n=10, firstTerm = 0, secondTerm = 1;
+//
+//        //혼자 단독으로 쓰이면 1증가 (++i 나 i++)
+//        for(int i=1; i<= 10; ++i) {
+//            System.out.print(firstTerm + ", ");
+//
+//            int nextTerm = firstTerm + secondTerm;
+//            firstTerm = secondTerm;
+//            secondTerm = nextTerm;
+//        }
+//        System.out.println(i);
+
+        //배열
+//        int a = 10;
+//
+//        String s = "안녕";
+//
+//        int[] arr = new int[3]; //참조타입 변수
+//
+//        int a = 10;
+//        int b = a;
+//
+//        int[] arr = new int[3];
+//        int[] arr2 = arr;
+//
+//        System.out.println(arr[0]);
+    }
+}
