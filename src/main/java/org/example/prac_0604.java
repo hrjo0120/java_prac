67597f5fc748384a334669e0d1460e150800cf31
깃hrjo0120 <hrjo0120@gmail.com>
2024-06-04 16:46:22 +0900

    0604-function

--- src/main/java/org/example/prac_0604.java
+++ src/main/java/org/example/prac_0604.java
@@ -2,8 +2,8 @@
 //오전
 package org.example;
 
-public class prac_0604 {
-    public static void main(String[] args) {    //메인메소드가 실행 기준
+//public class prac_0604 {
+//    public static void main(String[] args) {    //메인메소드가 실행 기준
         //오전
 //        사람 a = new 사람();
 //        a.age = 23;
@@ -18,47 +18,47 @@ public class prac_0604 {
 //        b.introduce();
 //        System.out.println(b.age);
 
-        //오후
-        //지금부터 하는 내용 중요함
-        //계산기.합();    //계산기 클래스 안에 있는 합이라는 메소드를 실행하겠다.
-        계산기.합(10, 20);    //출력: 30  소괄호 안에 값을 넣어줌. 소괄호 안은 요청사항으로 보면 됨
-        계산기.합(20, 50);    //출력: 70
-        계산기.합(30, 40);
-
-//        //안되는 예시
-//        //계산기.합();  //값을 안줘서 오류
-//        //계산기.합(true, 40);  //타입이 안맞아서 오류
-//        //계산기.합(30, 40, 40);  //갯수가 안맞아서 오류
-        계산기.합(9);
-
-        //System.out.println(a + b);  //a, b의 수명 , 메인 메소드 안에서는 a, b가 뭔지 알아 차릴 수 있는 능력이 없다.
-
-    }
-}
-
-//오후
-class 계산기 {
-    static void 합(int c) {
-        System.out.println(c);
-    }
-
-    static void 합(int a, int b) {
-//        int a = 30;     // 70밖에 출력이 안되는 이유: a와 b의 값이 고정이라서.
-//        int b = 40;
-//        int a;  //초기화 된 적이 없기때문에 문제가 된다.
-//        int b;
-        System.out.println("a: " + a);
-        System.out.println("b: " + b);
-    }
-    // 위에있는 두가지를 묶어 메소드 오버로딩 이라고 한다. (다형성의 한 종류)
-
-//    static void print_a_and_b() {
-//        System.out.println(a);  // 안되는 이유: 합 메소드의 지역변수로 a,b가 있기 때문에 알아차릴 수 있는 능력이 없다.
-//        System.out.println(b);
+//        //오후
+//        //지금부터 하는 내용 중요함
+//        //계산기.합();    //계산기 클래스 안에 있는 합이라는 메소드를 실행하겠다.
+//        계산기.합(10, 20);    //출력: 30  소괄호 안에 값을 넣어줌. 소괄호 안은 요청사항으로 보면 됨
+//        계산기.합(20, 50);    //출력: 70
+//        계산기.합(30, 40);
+//
+////        //안되는 예시
+////        //계산기.합();  //값을 안줘서 오류
+////        //계산기.합(true, 40);  //타입이 안맞아서 오류
+////        //계산기.합(30, 40, 40);  //갯수가 안맞아서 오류
+//        계산기.합(9);
+//
+//        //System.out.println(a + b);  //a, b의 수명 , 메인 메소드 안에서는 a, b가 뭔지 알아 차릴 수 있는 능력이 없다.
+//
 //    }
-
-
-}
+//}
+//
+////오후
+//class 계산기 {
+//    static void 합(int c) {
+//        System.out.println(c);
+//    }
+//
+//    static void 합(int a, int b) {
+////        int a = 30;     // 70밖에 출력이 안되는 이유: a와 b의 값이 고정이라서.
+////        int b = 40;
+////        int a;  //초기화 된 적이 없기때문에 문제가 된다.
+////        int b;
+//        System.out.println("a: " + a);
+//        System.out.println("b: " + b);
+//    }
+//    // 위에있는 두가지를 묶어 메소드 오버로딩 이라고 한다. (다형성의 한 종류)
+//
+////    static void print_a_and_b() {
+////        System.out.println(a);  // 안되는 이유: 합 메소드의 지역변수로 a,b가 있기 때문에 알아차릴 수 있는 능력이 없다.
+////        System.out.println(b);
+////    }
+//
+//
+//}
 
 
 //오전
--- src/main/java/org/example/prac_0604_function.java
+++ src/main/java/org/example/prac_0604_function.java
@@ -1,31 +1,62 @@
 package org.example;
 
 public class prac_0604_function {
-    static int a = 20;  //여기서 선언된 변수는 전역변수(why? 함수 밖에서 선언됨)
+//    static int a = 20;  //여기서 선언된 변수는 전역변수(why? 함수 밖에서 선언됨)
 
     public static void main(String[] args) {
-        System.out.println(a);
-        int a = 30;
-        System.out.println(a);
-        exam();
-        계산기.합(10, 20);
-        계산기.합(80, 90);
+//        System.out.println(a);
+//        int a = 30;
+//        System.out.println(a);
+//        exam();
+//        계산기.합(10, 20);
+//        계산기.합(80, 90);
+//        int rs = 10 + 30;
+//        System.out.println("rs : " + rs);
+//
+//        int rs2 = 계산기.합(10,20);
+//        System.out.println("rs2 : "+ rs2);
+//    }
+//
+//    public static void exam() {
+//        System.out.println("exam 출력 a : " + a);
+//    }
+
+        //===
+        // 함수의 기본 사용법
+        int rs = 계산기.합(10, 20);
+        System.out.println(rs);
+
+        boolean rs2 = 계산기.is_a_bigger_than_b(10, 20);   // 사실 이문장은 꽤 많은 것을 담고있다. 리턴타입, 클래스명,메소드명,매개변수 갯수 등등
+
+        System.out.println(rs2);
+        rs2 = 계산기.is_a_bigger_than_b(240, 20);
+        System.out.println(rs2);
+        rs2 = 계산기.is_a_bigger_than_b(20, 20);
+        System.out.println(rs2);
 
     }
 
-    public static void exam() {
-        System.out.println("exam 출력 a : " + a);
+    class 계산기 {
 
+//    static int 합(int a, int b) {
+//        System.out.println(a + b);
+//        return 10;
+//    }
+
+        static int 합(int a, int b) {
+            return a + b;
+        }
+
+        static boolean is_a_bigger_than_b(int a, int b) {
+            if(a > b) {
+                return true;
+            }
+
+            return false;
+
+        }
     }
+
 }
 
-class 계산기 {
-    int a = 40;
-
-    static void 합(int a, int b) {
-        System.out.println("계산기 클래스 합 메서드 내부 a: " + a);
-        System.out.println("계산기 클래스 합 메서드 내부 a: " + b);
-
-    }
-}
 
