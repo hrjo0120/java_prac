39f5c56e48f0b0518f69594c4a3e116a1cdabdec
깃hrjo0120 <hrjo0120@gmail.com>
2024-06-18 11:52:14 +0900

    0618

---
+++ src/main/java/org/example/prac_0618_2.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class prac_0618_2 {
+    // 문제 : 아래가 실행되도록 해주세요.
+    //무기 a무기 = new 칼();
+
+    // 문제 : 아래가 실행되도록 해주세요.
+    칼 a칼 = new 칼();
+    a칼 = new 활();
+
+    //무기 a무기 = new 칼();
+    //a무기.공격();
+    // 출력 : 칼로 공격합니다.
+}
+
+//class 무기 {
+//    // 버튼 유지용
+//    void 공격() { }
+//}
+
+//class 칼 extends 무기{
+//    void 공격() {
+//        System.out.println("칼로 공격합니다.");
+//    }
+//}
+
+class 칼 {
+
+}
+class 활 extends 칼{
+
+}
